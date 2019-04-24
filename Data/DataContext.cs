e4bfe9c46456592fd7db6680ecc22a23d33f8d72
Taylon Kalel <taylon.kalel@gmail.com>
2019-04-23 21:29:00 -0300

    Projeto iniciando com WebApi final da aula

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoAgil.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAgil.WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
